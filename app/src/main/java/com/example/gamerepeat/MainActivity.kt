81111fa887725da0893cede6a16d2d2f7b8d4f2d
artem <artemzonov21@gmail.com>
2023-03-08 13:19:40 +0200

    added game ui

--- app/src/main/java/com/example/gamerepeat/MainActivity.kt
+++ app/src/main/java/com/example/gamerepeat/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.gamerepeat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.gamerepeat.fragments.GameFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.flMain, GameFragment())
+            .commit()
     }
 }
