469e4a08a1488861bf88918534cf83fe3a874769
artem <artemzonov21@gmail.com>
2023-03-09 12:03:44 +0200

    added webView fragment

--- app/src/main/java/com/example/gamerepeat/MainActivity.kt
+++ app/src/main/java/com/example/gamerepeat/MainActivity.kt
@@ -1,23 +1,49 @@
 package com.example.gamerepeat
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
+import android.content.Context
+import android.net.ConnectivityManager
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.webkit.WebView
+import androidx.appcompat.app.AppCompatActivity
 import com.example.gamerepeat.fragments.GameFragment
 import com.example.gamerepeat.fragments.WebFragment
 
 class MainActivity : AppCompatActivity() {
+    //private val webView = findViewById<WebView>(R.id.webView)
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-//        supportFragmentManager
-//            .beginTransaction()
-//            .replace(R.id.flMain, GameFragment())
-//            .commit()
+//        if (savedInstanceState != null) {
+//            webView.restoreState(savedInstanceState)
+//        }else {
+//            webView.loadUrl("https://ohmytraff.space/api")
+//        }
 
-        supportFragmentManager
-            .beginTransaction()
-            .replace(R.id.flMain, WebFragment())
-            .commit()
+        isOnline(this)
+
     }
+    private fun isOnline(context: Context) {
+        val connectivityManager =
+            context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val networkInfo = connectivityManager.activeNetworkInfo
+        if (networkInfo != null && networkInfo.isConnectedOrConnecting) {
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.flMain, WebFragment())
+                .commit()
+        } else {
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.flMain, GameFragment())
+                .commit()
+        }
+    }
+//    override fun onSaveInstanceState(outState: Bundle) {
+//        webView.saveState(outState)
+//        super.onSaveInstanceState(outState)
+//    }
 }
--- app/src/main/java/com/example/gamerepeat/fragments/WebFragment.kt
+++ app/src/main/java/com/example/gamerepeat/fragments/WebFragment.kt
@@ -5,12 +5,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebSettings
+import android.webkit.WebView
 import android.webkit.WebViewClient
+import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
+import com.example.gamerepeat.R
 import com.example.gamerepeat.databinding.FragmentWebBinding
 
 class WebFragment : Fragment() {
-    private lateinit var binding : FragmentWebBinding
+    private lateinit var binding: FragmentWebBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -18,26 +22,43 @@ class WebFragment : Fragment() {
         binding = FragmentWebBinding.inflate(inflater, container, false)
         return binding.root
     }
-
+//        override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        //val webView = activity?.findViewById(R.id.webView) as WebView
+//        if (savedInstanceState != null) {
+//            binding.webView.restoreState(savedInstanceState)
+//        }else {
+//            binding.webView.loadUrl("https://ohmytraff.space/api")
+//        }
+//    }
     @SuppressLint("SetJavaScriptEnabled")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.webView.webViewClient = WebViewClient()
 
-        // this will load the url of the website
-        binding.webView.loadUrl("https://ohmytraff.space/api")
-        binding.webView.settings.javaScriptEnabled = true
-        binding.webView.settings.setSupportZoom(true)
+        binding.webView.apply {
+            webViewClient = WebViewClient()
+
+            if (savedInstanceState != null)
+                restoreState(savedInstanceState);
+            else
+                loadUrl("https://ohmytraff.space/api")
+
+            settings.apply {
+                javaScriptEnabled = true
+                cacheMode = WebSettings.LOAD_CACHE_ELSE_NETWORK
+                setSupportZoom(true)
+                textZoom = 100
+                loadsImagesAutomatically = true
+            }
+        }.canGoBack()
+
+        val callback = object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                if (binding.webView.canGoBack()) {
+                    binding.webView.goBack()
+                }
+            }
+        }
+        activity?.onBackPressedDispatcher?.addCallback(viewLifecycleOwner, callback)
     }
-//    override fun onBackPressed() {
-////        // if your webview can go back it will go back
-////        if (binding.webView.canGoBack()) {
-////            binding.webView.goBack()
-////            // if your webview cannot go back
-////            // it will exit the application
-////        } else {
-////            super.onBackPressed()
-////        }
-////    }
-
-}
+    }
